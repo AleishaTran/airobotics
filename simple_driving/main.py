081e19c7a928181099609c5bba6868adf11d792e
AleishaTran <145763892+AleishaTran@users.noreply.github.com>
2025-04-08 18:48:28 +1000

    Create main.py

---
+++ simple_driving/main.py
@@ -0,0 +1,14 @@
+class SimpleDrivingEnv(gym.Env):
+    metadata = {'render.modes': ['human']}  
+
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self):
+        pass
